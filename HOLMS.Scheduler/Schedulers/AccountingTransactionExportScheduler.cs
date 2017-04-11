0b64a4aeac710994ef8514b79057dda977da43f8
pcsouza <souza.patrick@gmail.com>
2017-04-11 15:01:39 -0700

    Removes immediate report parameter [#143386647]

--- HOLMS.Scheduler/Schedulers/AccountingTransactionExportScheduler.cs
+++ HOLMS.Scheduler/Schedulers/AccountingTransactionExportScheduler.cs
@@ -10,11 +10,9 @@ using Quartz;
 
 namespace HOLMS.Scheduler.Schedulers {
     class AccountingTransactionExportScheduler : TaskSchedulerBase {
-        const string ImmediateReportFlag = "--immediatereport";
 
         private readonly string _outputPath;
         private InclusiveCalendarDateRange _reportRange;
-        private bool _immediateReportRequested;
 
         public AccountingTransactionExportScheduler(ILogger logger, ISchedulerFactory sf)
                 : base(logger, sf) {
@@ -28,67 +26,12 @@ namespace HOLMS.Scheduler.Schedulers {
             // is mm/dd/yyyy.
             Logger.LogInformation("Parsing arguments in AccountingTransactionExportJobScheduler");
 
-            if (args.Contains(ImmediateReportFlag)) {
-                _immediateReportRequested = true;
-                _reportRange = ParseImmediateReportRange(args);
-                Logger.LogInformation($"Immediate report requested for date range: {_reportRange.Start} to {_reportRange.End}");
-            } else {
-                _immediateReportRequested = false;
-                Logger.LogInformation("Using nightly reporting schedule");
-            }
+            Logger.LogInformation("Using nightly reporting schedule");
         }
 
         public override void Schedule() {
-            if (_immediateReportRequested) {
-                Logger.LogInformation("Scheduling immediate reporting run for accounting transaction reporter");
-                ScheduleImmediateReport();
-            } else {
-                Logger.LogInformation("Scheduling nightly reporting run for accounting transaction reporter");
-                ScheduleNightlyReport();
-            }
-        }
-
-        private static InclusiveCalendarDateRange ParseImmediateReportRange(IReadOnlyList<string> args) {
-            DateTime start;
-            DateTime end;
-            int i = args.ToList().IndexOf(ImmediateReportFlag);
-
-            if (DateTime.TryParse(args[i + 1], out start) && DateTime.TryParse(args[i + 2], out end)) {
-                return new InclusiveCalendarDateRange(start, end);
-            } else {
-                return new InclusiveCalendarDateRange(DateTime.Today, DateTime.Today);
-            }
-        }
-
-        private void ScheduleImmediateReport() {
-            var basedata = new JobDataMap() {
-                {AccountingTransactionExportJob.OutputPathKey, _outputPath},
-                {AccountingTransactionExportJob.RunTypeKey, "Manual" }
-            };
-
-            var datedata = new JobDataMap() {
-                {AccountingTransactionExportJob.EndDateKey, _reportRange.EndDateTime},
-                {AccountingTransactionExportJob.StartDateKey, _reportRange.StartDateTime}
-            };
-
-            var job = JobBuilder
-                .Create<AccountingTransactionExportJob>()
-                .WithIdentity(AccountingTransactionExportJob.JobNameString,
-                    AccountingTransactionExportJob.JobGroupString)
-                .SetJobData(basedata)
-                .Build();
-
-            var trigger = TriggerBuilder.Create()
-                .WithIdentity("AccountingTransactionExportImmediate",
-                    AccountingTransactionExportJob.JobGroupString)
-                .ForJob(AccountingTransactionExportJob.JobNameString,
-                    AccountingTransactionExportJob.JobGroupString)
-                .UsingJobData(datedata)
-                .WithSimpleSchedule(x => x.WithInterval(new TimeSpan(0, 0, 0)).WithRepeatCount(0))
-                .Build();
-
-            var sched = SF.GetScheduler();
-            sched.ScheduleJob(job, trigger);
+            Logger.LogInformation("Scheduling nightly reporting run for accounting transaction reporter");
+            ScheduleNightlyReport();
         }
 
         private void ScheduleNightlyReport() {
