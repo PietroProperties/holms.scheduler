2788d5443be95b8c0d4dbaf6a40a8e0d46225788
David R. Albrecht <albrecht.dr@gmail.com>
2017-04-12 11:54:51 -0700

    Fix exception logging, so that scheduler writes exceptions to log

--- HOLMS.Scheduler/Jobs/QuartzJobBase.cs
+++ HOLMS.Scheduler/Jobs/QuartzJobBase.cs
@@ -17,7 +17,7 @@ namespace HOLMS.Scheduler.Jobs {
                 ac.Logger.LogInformation(JobName + " execution completed");
             } catch (Exception ex) {
                 var logger = JobEnvConstructor.GetLogger();
-                logger.LogError($"Caught unhandled exception in {JobGroup}:{JobName}", ex);
+                logger.LogError(new EventId(), ex, $"Caught unhandled exception in {JobGroup}:{JobName}");
                 logger.LogError("Re-throwing to abort the job");
 
                 throw;
